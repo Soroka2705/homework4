5184a9d73220f0dba0b744ae15315b3043a01c9f
soroka <sorokindv88@mail.ru>
2022-11-14 23:29:16 +0300

    homework 4 start

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+
+/*
+int Exponent(int a, int b)
+{
+    int result = 1;
+    for(int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+
+return result;
+}
+
+Console.Write("input a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("input b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int exponent = Exponent(a, b);
+Console.WriteLine("Answer: " + exponent);
+*/
+
+//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("input numberN");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int numberN)
+{
+    
+}
